0e75b1225f4abfcfeb3da190a291c46a6ba401ed
Eugenio Alonso <eugenio.alonso937@comunidadunir.net>
2021-07-17 04:36:00 +0200

    credentials?

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPCION ID>"
-  client_id       = "<APP_ID>"
-  client_secret   = "<PASSWORD>"
-  tenant_id       = "<TENANT>"
+  subscription_id = "50a95373-17ad-4eb2-9506-f2a06c084828"
+  client_id       = "ada277ad-db14-4b0a-b1ab-815a19f42226"
+  client_secret   = "UM8N~qLF_GTJrv_QJ9ELg07zmmGYM_TRRa"
+  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
 }
