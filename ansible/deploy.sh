1db4c2035a2dbc13a57b5bb7ce08f773a7a5c89d
Eugenio Alonso <eugenio.alonso937@comunidadunir.net>
2021-07-20 19:40:14 +0200

    ansible

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
+#Configuración inicial de las máquinas
+ansible-playbook -i hosts hosts-init.yaml
+#Configuración del servicio de almacenamiento servidor y clientes
+ansible-playbook -i hosts NFS-deploy.yaml
+#Despliegue de la instalación, configuración de master y join de workers
+ansible-playbook -i hosts k8s-deploy.yaml
